c7ef13ae8ee567b0a0ccac11d42a69953179e291
Podbeat <52011295+Podbeat@users.noreply.github.com>
2026-09-13 02:02:17 +0200

    Delete app directory

--- app/robots.js
+++
@@ -1,8 +0,0 @@
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://db90-com.vercel.app";
-
-export default function robots() {
-  return {
-    rules: [{ userAgent: "*", allow: "/", disallow: ["/admin", "/api"] }],
-    sitemap: `${SITE_URL}/sitemap.xml`,
-  };
-}
